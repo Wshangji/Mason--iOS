e5909309495955e8b94d259d248fad082890b25d
TongWYMAC <shangdtream@gmail.com>
2021-11-03 19:50:48 +0800

    add login model

--- Mason/LoginViewController.swift
+++ Mason/LoginViewController.swift
@@ -9,21 +9,31 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var errorLabel: UILabel!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    // The function judging Page is or not jump
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        
+        let username = usernameTextField.text ?? ""
+        let password = passwordTextField.text ?? ""
+        
+        if identifier == "LoginSuccess" {
+            if username.isEmpty || password.isEmpty {
+                errorLabel.text = "请输入用户名或密码"
+                return false
+            }
+            errorLabel.text = ""
+        }
+        return true
     }
-    */
 
 }
