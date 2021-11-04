7b0bcc1f71dd6ccd836482f2d76be0b2e5e47103
TongWYMAC <shangdtream@gmail.com>
2021-11-04 10:17:21 +0800

    add view

--- Mason/RegisterViewController.swift
+++ Mason/RegisterViewController.swift
@@ -8,7 +8,14 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
-
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmpwdTextField: UITextField!
+    
+    @IBOutlet weak var errorTable: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +23,30 @@ class RegisterViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // The function judging Page is or not jump
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        
+        let username = usernameTextField.text ?? ""
+        let password = passwordTextField.text ?? ""
+        let email = emailTextField.text ?? ""
+        
+//        if identifier == "Confirme" {
+//            if username.isEmpty || password.isEmpty {
+//                errorTable.text = "请输入用户名或密码"
+//                return false
+//            }
+//
+//            if password != confirmpwdTextField.text {
+//                errorTable.text = "密码不一致"
+//                return false
+//            }
+//            if email.isEmpty {
+//                errorTable.text = "请输入邮箱"
+//                return false
+//            }
+//            errorTable.text = ""
+//        }
+        return true
     }
-    */
 
 }
